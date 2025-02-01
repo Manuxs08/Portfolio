d3a1a844e0c148a3170522f9d897fbe8741ee657
Manuxs08 <manuelarotinco@gmail.com>
2025-01-31 21:12:45 -0500

    Page V3

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div id='footer'>
         <div>Contactos:</div>
-        <a rel='noopener noreferrer' target='_blank' href="http://discord.com/users/712691418581172326"><img src="/discordIcon.svg" alt="" /></a>
-        <a rel='noopener noreferrer' target='_blank' href="https://x.com/Manuxs_08"><img src="/twitterIcon.svg" alt="" /></a>
+        <a rel='noopener noreferrer' target='_blank' href="http://discord.com/users/712691418581172326"><img className='icon-footer' src="/discordIcon.svg" alt="" /></a>
+        <a rel='noopener noreferrer' target='_blank' href="https://x.com/Manuxs_08"><img className='icon-footer' src="/twitterIcon.svg" alt="" /></a>
     </div>
   )
 }
